4702db173ce52a033052b3b03fe51fe1402d2afa
mehmetcevikdev <mehmet_cvk20@outlook.com>
2025-12-21 19:44:56 +0300

    adding file loading

--- AI Chatbot project/script.js
+++ AI Chatbot project/script.js
@@ -2,12 +2,18 @@ const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
 const sendMessageButton = document.querySelector("#send-message");
 const fileInput = document.querySelector("#file-input");
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
+const fileCancelButton = document.querySelector("#file-cancel");
 
 const API_KEY = "";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
   message: null,
+  file: {
+    data: null,
+    mime_type: null,
+  },
 };
 
 const createMessageElement = (content, ...classes) => {
@@ -26,7 +32,10 @@ const generateBotResponse = async (incomingMessageDiv) => {
     body: JSON.stringify({
       contents: [
         {
-          parts: [{ text: userData.message }],
+          parts: [
+            { text: userData.message },
+            ...(userData.file.data ? [{ inline_data: userData.file }] : []),
+          ],
         },
       ],
     }),
@@ -45,6 +54,7 @@ const generateBotResponse = async (incomingMessageDiv) => {
     messageElement.innerHTML = error.message;
     messageElement.style.color = "#ff0000";
   } finally {
+    userData.file = {};
     incomingMessageDiv.classList.remove("thinking");
     chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
   }
@@ -54,8 +64,14 @@ const handleOutgoingMessage = (e) => {
   e.preventDefault();
   userData.message = messageInput.value.trim();
   messageInput.value = "";
+  fileUploadWrapper.classList.remove('file-uploaded')
 
-  const messageContent = `<div class="message-text"></div>`;
+  const messageContent = `<div class="message-text"></div>
+                          ${
+                            userData.file.data
+                              ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="attachment" />`
+                              : ""
+                          }`;
 
   const outgoingMessageDiv = createMessageElement(
     messageContent,
@@ -104,11 +120,31 @@ messageInput.addEventListener("keydown", (e) => {
   }
 });
 
+// handle file input change
 fileInput.addEventListener("change", () => {
   const file = fileInput.files[0];
   if (!file) return;
-  
-  
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    fileUploadWrapper.querySelector("img").src = e.target.result;
+    fileUploadWrapper.classList.add("file-uploaded");
+    const base64String = e.target.result.split(",")[1];
+
+    // store file data and user data
+    userData.file = {
+      data: base64String,
+      mime_type: file.type,
+    };
+
+    fileInput.value = "";
+  };
+  reader.readAsDataURL(file);
+});
+
+fileCancelButton.addEventListener("click", () => {
+  userData.file={}
+  fileUploadWrapper.classList.remove('file-uploaded')
 });
 
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
